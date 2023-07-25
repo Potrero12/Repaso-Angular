1f94390ff154c7aac4f23c8e1b8734f06647aba0
Potrero12 <julian.12040@gmail.com>
2023-07-25 09:53:53 -0500

    directiva-nfor-ndswitch-y-ngClass

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -10,17 +10,54 @@ import { Zapatilla } from '../models/Zapatilla.model';
 export class ZapatillasComponent implements OnInit{
   public titulo:string;
   public zapatillas:Array<Zapatilla> = [];
+  public marcas:Array<String> = [];
+  public color:string;
+  public miMarca:string = '';
 
   constructor(){
     this.titulo = 'Zapatillas';
+    this.color = 'yellow';
+    this.marcas = new Array();
     this.zapatillas = [
       new Zapatilla('Nike', 200, 'Nike', 'Azul', true),
       new Zapatilla('Jordan', 600, 'Jordan', 'Rojas', true),
-      new Zapatilla('Reebook', 100, 'Reebook', 'Negras', false)
+      new Zapatilla('Reebook', 60, 'Reebook', 'Negras', true),
+      new Zapatilla('Jeeze Spartan', 50, 'Jeeze', 'Negras', true)
     ];
   }
 
   ngOnInit(): void {
-    
+    this.getMarcas();
+  }
+
+  getMarcas(){
+    this.zapatillas.forEach((zapatilla) => {
+      if(this.marcas.indexOf(zapatilla.marca) < 0){
+        this.marcas.push(zapatilla.marca);
+      }
+    })
+
+    console.log(this.marcas);
+  }
+
+  getMarca(){
+    alert(this.miMarca);
+  }
+
+  addMarca(){
+      this.marcas.push(this.miMarca);
+      this.miMarca = '';
+  }
+
+  borrarMarca(indice:any){
+    this.marcas.splice(indice, 1); //borrar elementos de un array
+  }
+
+  onBlur(){
+    console.log('haz salido del input');
+  }
+
+  onKeyUpEnter(){
+    alert(this.miMarca);
   }
 }
